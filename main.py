926b8d11061a9d28852b4d34eb2994aca6bda9a7
Yurij Hr <yuspax.pve@gmail.com>
2023-02-27 18:09:53 +0100

    solve merge conflict

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ server_id = 'abc123'
 
 if server_id == 'qqq':
     print('done')
+
+
+login =input('Enter login: ')
+password = input('Enter password: ')
+if login == 'abc' and password == '123':
+    print('SUCCESS')
